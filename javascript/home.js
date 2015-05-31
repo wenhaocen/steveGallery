d6dba15e3f14495a6ac080b3d61e519f994333fd
Wenhao Cen <wcen@elance-odesk.com>
2015-05-31 13:52:51 -0700

    fixing gallery

--- javascript/home.js
+++ javascript/home.js
@@ -3,30 +3,55 @@ $( document ).ready(function() {
   addGalleryEvents();
 });
 
+//If animation is in progress, we set it to true, so that we dont't re-initialize the animation again.
+galleryEventTracker = {};
 
 function addGalleryEvents(){
 
 	$(".img-container img").mouseover(function(event){
-		$("[src=\"" + event.target.getAttribute("src") + "\"]").css( "zIndex", 1000 );
-		$("[src=\"" + event.target.getAttribute("src") + "\"]").animate(
+                                      
+        var imageIdSrc = event.target.getAttribute("src");
+        var imageElem = $("[src=\"" + imageIdSrc + "\"]");
+        if (galleryEventTracker[imageIdSrc]){
+            //if true, that means some animation of this image already in progress, we simply return.
+            return;
+        }
+        //if we initialize the animation, then we set it to true
+        galleryEventTracker[imageIdSrc] = true;
+        
+		imageElem.css( "zIndex", 1000 );
+		imageElem.animate(
 		{
-			"width" : "320px",
-			"height" : "320px",
-			"top" : "-50px",
-			"left" : "-50px"
-		}, 500, function(){}
-		);
+			"width" : "260px",
+			"height" : "260px",
+			"top" : "-25px",
+			"left" : "-25px"
+		},
+        {
+            "duration" : "300",
+            "always" : function(){
+                //set it back to false once the animation is done
+                galleryEventTracker[imageIdSrc] = false;
+            }
+        }
+        
+        );
 	});
 
 	$(".img-container img").mouseleave(function(event){
-		$("[src=\"" + event.target.getAttribute("src") + "\"]").css( "zIndex", 1);
-		$("[src=\"" + event.target.getAttribute("src") + "\"]").animate(
+    
+        var imageIdSrc = event.target.getAttribute("src");
+        //if there is any animation triggered by the mouseover event(enlarging), stop it, then trigger the mouse leave animation(decreaseing the size).
+        var imageElem = $("[src=\"" + imageIdSrc + "\"]");
+        imageElem.stop();
+		imageElem.css( "zIndex", 1);
+		imageElem.animate(
 		{
 			"width" : "220px",
 			"height" : "220px",
 			"top" : "0px",
 			"left" : "0px"
-		}, 1, function(){}
+		}, 100, function(){}
 		);
 		
 	})
